4bcd2f87395c069cf7fb126d476a87960ed2c114
Anondo Ray <Anondo554@gmail.com>
2025-02-17 21:21:45 +0600

    basic setup done

---
+++ src/App.jsx
@@ -0,0 +1,13 @@
+ 
+import './App.css'  
+function App() {
+  return (
+    <>
+      <div className="text-6xl text-red-500 font-bold text-center mt-5">
+        <h1>Coffee Store</h1>
+      </div>
+    </>
+  );
+}
+
+export default App;
